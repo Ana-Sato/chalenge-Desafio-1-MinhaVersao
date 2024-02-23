d5119a8dd903de5af0b7382059c20fd9e98807ec
Ana Sato <anasato.est@gmail.com>
2024-02-22 22:00:46 -0300

    Update

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,11 @@ function criptografar() {
 
   let texto = textInput.value;
 
-  let resultCripto = texto.replace(/e/g, "enter")
-  .replace(/i/g, "imes")
-  .replace(/a/g, "ai")
-  .replace(/o/g, "ober")
-  .replace(/u/g, "ufat");
+  let resultCripto = texto.replace("e", "enter")
+                          .replace("i", "imes")
+                          .replace("a", "ai")
+                          .replace("o", "ober")
+                          .replace("u", "ufat");
 
   document.getElementById('container__mensagem').innerHTML = '<text-area readonly id="msgCripto">' + resultCripto + 
   '</text-area>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
@@ -22,14 +22,14 @@ function descriptografar() {
 
     let texto = textInput.value;
 
-    let resultDescripto = texto.replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ai/g, "a")
-    .replace(/ober/g, "o")
-    .replace(/ufat/g, "u");
+    let resultDescripto = texto.replace("enter", "e")
+                                .replace("imes", "i")
+                                .replace("ai", "a")
+                                .replace("ober", "o")
+                                .replace("ufat", "u");
   
     document.getElementById('container__mensagem').innerHTML = '<text-area readonly id="msgCripto">' + resultDescripto + 
-    '</text-area>' + '<button class="btn-copiar" id="btn-descripto" onclick="copiar()">Copiar</button>'
+        '</text-area>' + '<button class="btn-copiar" id="btn-descripto" onclick="copiar()">Copiar</button>';
 
    outInput.classList.add('auto-height');
 }
@@ -40,7 +40,7 @@ function copiar() {
   let inputTemporario = document.createElement("textarea");
   inputTemporario.value = textoCop;
   document.body.appendChild(inputTemporario);
-  
+
   inputTemporario.select();
   document.execCommand('copy');
   document.body.removeChild(inputTemporario);
